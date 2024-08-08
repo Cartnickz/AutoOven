2853feffbd94c59e823ed7565c815fddf77ae8b0
Zach Cartnick <zachcartnick@gmail.com>
2024-08-08 01:23:08 -0400

    code compiles!

--- Testing/Testing.ino
+++ Testing/Testing.ino
@@ -100,3 +100,23 @@ void loop() {
   delay(1000);
 
 }
+
+
+
+// timer functions
+time_t getTeensy3Time() {
+  return Teensy3Clock.get();
+}
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+unsigned long processSyncMessage() {
+  unsigned long pctime = 0L;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013 
+  if(Serial.find(TIME_HEADER)) {
+     pctime = Serial.parseInt();
+     return pctime;
+     if( pctime < DEFAULT_TIME) { // check the value is a valid time (greater than Jan 1 2013)
+       pctime = 0L; // return 0 to indicate that the time is not valid
+     }
+  }
+  return pctime;
+}
