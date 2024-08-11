0e26901f0450979b351f329120f35a243b3a1ab1
Zach Cartnick <zachcartnick@gmail.com>
2024-08-11 00:39:33 -0400

    Cleaning code + screen optimizations

--- Testing/Testing.ino
+++ Testing/Testing.ino
@@ -1,4 +1,4 @@
-//CartnickLabs
+// CartnickLabs
 
 // import libraries
 #include "TimeLib.h" // for time keeping
@@ -19,7 +19,6 @@ int MOSI_2_PIN = 50;  // not sure how to actually access these...
 int MISO_2_PIN = 54;
 int SCLK_2_PIN = 45;
 
-
 // initialize variables for MAX6675s
 int thermoCLK = 37;
 int thermoDO = 33;
@@ -29,6 +28,9 @@ int roomCS = 34;
 float meatTemp = 0;
 float ovenTemp = 0;
 float roomTemp = 0;
+float lastMeatTemp = 0;
+float lastOvenTemp = 0;
+float lastRoomTemp = 0;
 
 MAX6675 meatThermocouple(thermoCLK, meatCS, thermoDO);
 MAX6675 ovenThermocouple(thermoCLK, ovenCS, thermoDO);
@@ -49,39 +51,126 @@ Arduino_GFX *gfx = new Arduino_ILI9488_18bit(bus, screenRST, 1);
 // initialize variables for the timer
 int startTime = 0;
 int runTime = 0;
+int lastRunTime = 0;
+elapsedMillis thermoTimer;
+elapsedMillis serialOutTimer;
+elapsedMillis secondsTimer;
 
 // initialize other variables
 float tempSet = 0;
+
 
 void setup() {
   // setup Serial
   Serial.begin(115200);
   delay(1000);
 
-  // setup other pins
+  // screen
+  screenInit();
+  countdownDisplay(30);
+  
+  // timer
+  rtcInit();
+  startTime = now();
+}
 
-  // setup MAX6675s
 
-  // setup screen
+void loop() {
+  // loop timer
+  runTime = now()-startTime;
+
+  // gather temperatures and update screen
+  if (thermoTimer >= 500) {
+    meatTemp = meatThermocouple.readFahrenheit();
+    ovenTemp = ovenThermocouple.readFahrenheit();
+    roomTemp = roomThermocouple.readFahrenheit();
+    updateDisplayTemps();
+    thermoTimer -= 500;
+  }
+
+  // update screen display
+  if (secondsTimer >= 1000) {
+    updateDisplaySeconds();
+    secondsTimer -= 1000;
+  }
+
+  // print log 
+  if (serialOutTimer >= 1000) {
+    serialOutTemps();
+    serialOutTimer -= 1000;
+  }
+
+}
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ |  
+ | Important junk below this block.
+ |
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+void updateDisplayTemp(int x, int y, float lastTemp, float currentTemp) {
+  gfx->setCursor(x, y);                      
+  gfx->setTextColor(BLACK);
+  gfx->print(String(lastTemp) + " F");
+  gfx->setTextColor(WHITE);
+  gfx->print(String(currentTemp) + " F");
+  lastTemp = currentTemp;
+}
+
+
+void updateDisplayTemps() {
+  updateDisplayTemp(10, 10, lastMeatTemp, meatTemp);
+  updateDisplayTemp(150, 10, lastOvenTemp, ovenTemp);
+  updateDisplayTemp(300, 10, lastRoomTemp, roomTemp);
+}
+
+
+void serialOutTemps() {
+  Serial.print(runTime);
+  Serial.print("\t");
+  Serial.print(meatTemp);
+  Serial.print("\t");
+  Serial.print(ovenTemp);
+  Serial.print("\t");
+  Serial.println(roomTemp);
+}
+
+
+void updateDisplaySeconds() {
+  gfx->setCursor(10, 300);                      
+  gfx->setTextColor(BLACK);
+  gfx->print(String(runTime) + "s");
+  gfx->setTextColor(WHITE);
+  gfx->print(String(lastRunTime) + "s");
+  lastRunTime = runTime;
+}
+
+void screenInit(){
   gfx->begin();
   gfx->fillScreen(BLACK);
   pinMode(screenBL, OUTPUT);
   digitalWrite(screenBL, HIGH);
   gfx->setTextSize(2);
-  gfx->setCursor(10, 10);
+  gfx->setCursor(100, 10);
   gfx->setTextColor(RED);
-  gfx->println("Running...");
-  
-  // countdown start
-  for (int i = 30; i > 0; i--) {
-    gfx->fillRect(0, 0, 100, 100, BLACK);
-    gfx->setCursor(10, 10);
-    gfx->setTextColor(WHITE);
-    gfx->print(i);
-    delay(1000);
-  }
+  gfx->println("Loading...");
+}
 
-  // setup timer
+void countdownDisplay(int seconds){
+    for (int i = seconds; i > 0; i--) {
+      gfx->setCursor(10, 10);
+      gfx->setTextColor(WHITE);
+      gfx->print(i);
+      delay(1000);
+      gfx->setTextColor(BLACK);
+      gfx->print(i);
+  }
+}
+
+
+void rtcInit() {
   setSyncProvider(getTeensy3Time);
   if (Serial.available()) {
     time_t t = processSyncMessage();
@@ -90,76 +179,13 @@ void setup() {
       setTime(t);
     }
   }
-  startTime = now();
-  Serial.print("Time started: ");
-  Serial.print(startTime);
-  Serial.println(" s.");
-
 }
 
-void loop() {
-  // loop timer
-
-  runTime = now()-startTime;
-  Serial.print(runTime);
-  Serial.print("\t");
-
-  // loop for MAX6675s
-  meatTemp = meatThermocouple.readFahrenheit();
-  ovenTemp = ovenThermocouple.readFahrenheit();
-  roomTemp = roomThermocouple.readFahrenheit();
-  
-  Serial.print(meatTemp);
-  Serial.print("\t");
-  Serial.print(ovenTemp);
-  Serial.print("\t");
-  Serial.println(roomTemp);
-
-  // other loops
-  tempSet = floor(analogRead(A9) * 400 / 842 + 100);
-
-
-
-
-  // loop screen
-  gfx->fillRect(0, 0, 400, 200, BLACK);
-  gfx->setCursor(10, 10);
-  gfx->setTextColor(WHITE);
-  gfx->print(meatTemp);
-  gfx->print(" F");
-  gfx->setCursor(150, 10);
-  gfx->print(ovenTemp);
-  gfx->print(" F");
-  gfx->setCursor(300, 10);
-  gfx->print(roomTemp);
-  gfx->print(" F");
-  gfx->setCursor(10, 100);
-  gfx->setTextColor(WHITE);
-  gfx->print(runTime);
-  gfx->println(" s");
-  gfx->setCursor(150, 100);
-  if (ovenTemp < (tempSet - 10)) {
-    gfx->setTextColor(ORANGE);
-  } else if (ovenTemp > (tempSet + 10)) {
-    gfx->setTextColor(RED);
-  } else {
-    gfx->setTextColor(GREEN);
-  }
-  gfx->print(int(tempSet));
-  gfx->println(" F");
-  gfx->setTextColor(WHITE);
-  
-  // loop delay
-  delay(1000);
-
-}
-
-
-
-// timer functions
 time_t getTeensy3Time() {
   return Teensy3Clock.get();
 }
+
+
 #define TIME_HEADER  "T"   // Header tag for serial time sync message
 unsigned long processSyncMessage() {
   unsigned long pctime = 0L;
